31381dcec53e1446a4ea0d0f2b466ec0031da639
RobertooFelipe <feliperoberto092@gmail.com>
2022-11-23 12:32:48 -0300

    feat: adicionando conexao para SQL Server

--- src/migration/index.js
+++ src/migration/index.js
@@ -1,20 +1,35 @@
-const { Pool } = require("pg");
 const {
   DB_USER,
   DB_PASSWORD,
-  DB_HOST,
-  DB_PORT,
+  DB_SERVER,
   DB_DATABASE,
 } = require("dotenv/config");
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
-  host: process.env.DB_HOST,
-  ssl: true,
-  rejectUnauthorized: (process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"),
+const { Connection } = require("tedious");
+
+var config = {
+  server: process.env.DB_SERVER,
+  authentication: {
+    type: "default",
+    options: {
+      userName: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+  },
+  options: {
+    encrypt: false,
+    trustServerCertificate: true,
+    database: process.env.DB_DATABASE,
+  },
+};
+
+const connection = new Connection(config);
+
+connection.connect(function (err) {
+  if (err) {
+    console.log("Connection Failed!");
+    throw err;
+  }
 });
 
-module.exports = { pool };
+module.exports = { config };
